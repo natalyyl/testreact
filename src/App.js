700d8f2c3e4f279b1a68e55d1b53e383851cf050
Nataly <naty7160@gmail.com>
2024-07-22 20:03:56 -0300

    teste de parallax com CSS

--- src/App.js
+++ src/App.js
@@ -2,9 +2,20 @@ import './App.css';
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Estudos de React</h1>
-    </div>
+    <body>
+      <div className="App">
+       <h1>Estudos de React</h1>
+      </div> 
+      <div class= "color-img">
+        <p>#Adoramos, programar!!
+          beijos de naty e da vava
+        </p>
+      </div>
+      <div class= "img">
+        <textarea class="bloco"></textarea>
+      </div>
+    </body>
+       
   );
 }
 
